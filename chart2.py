1f8f398c2679b44c29bd3caa792a5ba9f9507724
asenachin <senachin.ap@yandex.ru>
2022-07-22 12:31:49 +0700

    Добавление графиков

---
+++ chart2.py
@@ -0,0 +1,21 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+matplotlib.use('Qt5Agg')
+
+def plot_chart():
+    
+    # Данные для графика
+    mean = 0
+    sigma = 1
+    x = np.arange(-5,5,.01)
+    f = np.exp(-np.square((x-mean)/sigma)/2)/(np.sqrt(2*np.pi)*sigma)
+    
+    fig, ax = plt.subplots()
+    ax.plot(x, f)
+    
+    ax.set(xlabel='X', ylabel='Y', title='Нормальное распределение')
+    ax.grid()
+    
+    plt.show()
